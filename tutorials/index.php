43aa56ba43d233b8d996e1f78aa3f0ba0973d271
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2021-01-03 09:39:30 +0000

    defining constant variables with difine() functions

--- tutorials/index.php
+++ tutorials/index.php
@@ -1,7 +1,11 @@
  <?php
+    // We can override this variable so its not constant. To difine a vairable and make it remain as that value indefinately we use a constanint - We use a function called define. Commone practice to write vairiable in capital  letters so we know its constant when we use it in code. Then we set the value of that constant. 
+    define('NAME', 'Yoshi');
 
-    $name = 'Yoshi';
+    // $name = 'Yoshi'; 
     $age = 30;
+
+    //$name = 'Mario'
 
     ?>
 
@@ -17,7 +21,8 @@
 
      <h1>User Profile Page</h1>
 
-     <div><?php echo $name; ?></div>
+     <div><?php echo NAME; ?></div>
+     <div><?php echo $age; ?></div>
  </body>
 
  </html>
