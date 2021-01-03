cadbad028fade0d8581b5e594a9a45061f65c34b
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2021-01-03 09:23:24 +0000

    PHP syntax

---
+++ tutorials/index.php
@@ -0,0 +1,23 @@
+ <?php
+
+    $name = 'Yoshi';
+    $age = 30;
+
+    ?>
+
+ <!DOCTYPE html>
+ <html>
+
+ <head>
+
+     <title>PHP Tutorials</title>
+ </head>
+
+ <body>
+
+     <h1>User Profile Page</h1>
+
+     <div><?php echo $name; ?></div>
+ </body>
+
+ </html>
